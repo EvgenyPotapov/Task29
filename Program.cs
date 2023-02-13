15d9017a4b7fbc22ffc8e1bc6fea99641ecd6b68
Evgenii Potapov <evgeny-potapov@mail.ru>
2023-02-13 19:48:28 +0300

    рещение задачи

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿//Напишите программу, которая задаёт массив из 8
+// элементов и выводит их на экран.
+
+
+Console.Clear();
+
+int[] FillArray()
+{
+    int[] array = new int[8];
+    int lengtn = array.Length;
+    int index = 0;
+    while (index < lengtn)
+    {
+        array[index] = new Random().Next(1, 99);
+        index++;
+    }
+    return array;
+}
+
+void PrintArray(int[] number)
+{
+    int length = number.Length;
+    int index = 0;
+    Console.Write("[");
+    while (index < length)
+    {
+        Console.Write(number[index]);
+        if (index != length - 1)
+        {
+            Console.Write(", ");
+        }
+        index++;
+    }
+    Console.Write("]");
+}
+
+void IfPrintArray(int[] number)
+{
+    int length = number.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Console.Write(number[index]);
+        if (index != length - 1) ;
+        {
+            Console.Write(", ");
+        }
+        index++;
+    }
+}
+
+int[] array = FillArray(); 
+
+
+IfPrintArray(array);
+Console.Write(" -> ");
+PrintArray(array);
